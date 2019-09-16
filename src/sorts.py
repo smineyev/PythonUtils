851b1ab5ef31d64fdf69927db9a9472f1cf4c177
Sergey Mineyev <sergey@androidworks.com>
2019-09-16 15:02:37 -0700

    heap sort

--- src/sorts.py
+++ src/sorts.py
@@ -56,12 +56,64 @@ def __quickSort(array, start_idx: int, end_idx: int):
 def quickSort(array):
     __quickSort(array, 0, len(array)-1)    
     
+def __pushTailUp(a, end_idx):
+    if end_idx == 0:
+        #at the root
+        return
+    v = a[end_idx]
+    parent_idx = (end_idx-1)//2
+    parent_val = a[parent_idx]
+    if parent_val<v:
+        a[end_idx] = a[parent_idx]
+        a[parent_idx] = v
+        __pushTailUp(a, parent_idx)
+        
+
+def __sinkHeadDown(a, node_idx, end_idx):
+    min_idx = -1
+    left_child_idx = 2*node_idx+1
+    if left_child_idx > end_idx:
+        return
+    right_child_idx = 2*node_idx+2
+    if right_child_idx > end_idx:
+        if a[left_child_idx] > a[node_idx]:
+            min_idx = left_child_idx
+    else:
+        if a[left_child_idx] > a[right_child_idx]:
+            if a[left_child_idx] > a[node_idx]:
+                min_idx = left_child_idx
+        else:
+            if a[right_child_idx] > a[node_idx]:
+                min_idx = right_child_idx
+                
+    if not min_idx == -1:
+        __swap(array, min_idx, node_idx)
+        __sinkHeadDown(a, min_idx, end_idx)
+    
+
+
+def heapSort(a):
+    #build heap
+    for i in range(1, len(a)):
+        __pushTailUp(a, i)
+    print(a)
+    for i in range(len(a)-1, 0, -1):
+        __swap(a, 0, i)
+        print(a)
+        __sinkHeadDown(a, 0, i-1)
+        print(a)
+    #peak from heap
+    
             
             
     
-array=[4, 2, 7, 9, 3, 2, 1, 8, 5, 1, 5]
+array=[4, 2, 7, 9, 3, 2, 0, 8, 5, 1]
+# array=[4, 2, 7, 9]
 
 print ("Original: "+str(array))
-quickSort(array)
+heapSort(array)
 print ("Result: "+str(array))
-        
+
+# for i, elem in enumerate(array):
+#     print ("%s -> %s"%(elem, array[(i-1)//2]))
+
